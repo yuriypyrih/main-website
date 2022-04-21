891c744a3a0b4d33737d12621a78dd0ef6de7c3a
Yuriy <yuriypyrih@gmail.com>
2022-04-21 13:58:29 +0300

    fix(DodgePage): video mp4 format

---
+++ src/components/dodgeSections/SectionDodgeIntro.tsx
@@ -0,0 +1,91 @@
+import React from "react";
+import {
+  Box,
+  Button,
+  Container,
+  Grid,
+  IconButton,
+  Typography,
+} from "@mui/material";
+import ReactPlayer from "react-player";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+
+const SectionDodgeIntro: React.FC = () => {
+  return (
+    <Box
+      sx={{
+        minHeight: "calc(100vh - 80px)",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+      }}
+    >
+      <Container
+        maxWidth={"md"}
+        sx={{
+          position: "relative",
+        }}
+      >
+        <Grid container sx={{ maxWidth: 800, margin: "0 auto" }}>
+          <Grid item xs={12}>
+            <Typography
+              sx={{ color: "white", fontSize: 18, textAlign: "center" }}
+            >
+              {`So this is the Dodge Project So this is the Dodge Project So this is the
+                     Dodge Project So this is the sdasd saadsa dasdsad asdsad jdks jksjdks 
+                      dksd Dodge Project So this is the Dodge Project`}
+            </Typography>
+          </Grid>
+          <Grid item xs={12} container justifyContent={"center"} sx={{ mt: 3 }}>
+            <Grid
+              item
+              sx={{
+                maxWidth: "700px",
+                flex: 1,
+              }}
+            >
+              <div
+                style={{
+                  position: "relative",
+                  paddingBottom: "55.25%", // ADJUST THIS WHEN YOU ARE READY
+                }}
+              >
+                {/* @ts-ignore*/}
+                <ReactPlayer
+                  url="/video/dodge-preview.mp4"
+                  width={"100%"}
+                  height={"100%"}
+                  className={"reactPlayerCustomClass"}
+                  playing={true}
+                  muted={true}
+                  loop={true}
+                />
+              </div>
+            </Grid>
+          </Grid>
+          <Grid item xs={12} container justifyContent={"center"} sx={{ mt: 3 }}>
+            <Button sx={{ p: 2, border: "2px solid grey" }}>Play Game</Button>
+          </Grid>
+          <Grid item xs={12} container sx={{ mt: 5 }}>
+            <Grid item xs={12}>
+              <Typography
+                sx={{ color: "white", fontSize: 18, textAlign: "center" }}
+              >
+                {`Or Scroll Down to learn more`}
+              </Typography>
+            </Grid>
+            <Grid item xs={12} container justifyContent={"center"}>
+              <IconButton>
+                <ExpandMoreIcon
+                  sx={{ width: "40px", height: "40px", color: "white" }}
+                />
+              </IconButton>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Container>
+    </Box>
+  );
+};
+
+export default SectionDodgeIntro;
--- src/pages/DodgePage.tsx
+++ src/pages/DodgePage.tsx
@@ -1,8 +1,8 @@
 import React from "react";
-import { Box, Container, Grid, Typography } from "@mui/material";
+import { Box } from "@mui/material";
 import Header from "../components/structure/Header";
 import Footer from "../components/structure/Footer";
-import ReactPlayer from "react-player";
+import SectionDodgeIntro from "../components/dodgeSections/SectionDodgeIntro";
 
 const DodgePage: React.FC = () => {
   return (
@@ -18,60 +18,7 @@ const DodgePage: React.FC = () => {
           overflow: "auto",
         }}
       >
-        <Box
-          sx={{
-            minHeight: "calc(100vh - 100px)",
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "center",
-          }}
-        >
-          <Container
-            maxWidth={"md"}
-            sx={{
-              position: "relative",
-            }}
-          >
-            <Grid container>
-              <Grid item xs={12}>
-                <Typography
-                  sx={{ color: "white", fontSize: 18, textAlign: "center" }}
-                >
-                  {
-                    "So this is the Dodge Project So this is the Dodge Project So this is the Dodge Project So this is the Dodge Project So this is the Dodge Project"
-                  }
-                </Typography>
-              </Grid>
-              <Grid item xs={12} container justifyContent={"center"}>
-                <Grid
-                  item
-                  sx={{
-                    maxWidth: "700px",
-                    flex: 1,
-                  }}
-                >
-                  <div
-                    style={{
-                      position: "relative",
-                      paddingBottom: "55.25%", // ADJUST THIS WHEN YOU ARE READY
-                    }}
-                  >
-                    {/* @ts-ignore*/}
-                    <ReactPlayer
-                      url="/video/dodge-preview.mov"
-                      width={"100%"}
-                      height={"100%"}
-                      className={"reactPlayerCustomClass"}
-                      playing={true}
-                      muted={true}
-                      loop={true}
-                    />
-                  </div>
-                </Grid>
-              </Grid>
-            </Grid>
-          </Container>
-        </Box>
+        <SectionDodgeIntro />
         <Footer />
       </Box>
     </Box>
